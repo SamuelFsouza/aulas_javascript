40cbb001954ad4ad667f899562415330f659785b
SAMUEL FORTUNATO DE SOUZA <samuel_f_de-souza@estudante.sesisenai.org.br>
2026-09-16 22:18:16 -0300

    Exemplos

---
+++ Aula 01/exemplo13.js
@@ -0,0 +1,8 @@
+let total = somarNumeros (34,33);
+console.log(`O total da soma é: ${total}`);
+
+function somarNumeros(num1, num2) {
+    let resultado = num1 + num2;
+    return resultado; // opcional
+  };
+  
---
+++ Aula 01/exemplo14.js
@@ -0,0 +1,5 @@
+const subtrair = function(a, b) {
+  return a - b;
+};
+
+console.log(`O total é: ${subtrair(10,3)}`)
